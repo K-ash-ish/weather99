e542fbf820120d7fca2c7c638bf5689ac12ec2a5
K-ash-ish <kashishsondhiya@gmail.com>
2023-11-29 16:52:50 +0530

    updating font to ROBOTO

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
-import { Inter } from "next/font/google";
+import { Inter, Roboto } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Roboto({ subsets: ["latin"], weight: ["400", "700", "300"] });
 
 export const metadata = {
   title: "Create Next App",
